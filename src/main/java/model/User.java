4846b46fd15876435d41c9aa5d88b3a779342aa8
ViictorHugoo <victorhugorodriguesguimaraes@gmail.com>
2025-06-25 13:27:27 -0300

    remove password

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -5,23 +5,17 @@ import java.util.ArrayList;
 
 public class User {
     private String name;
-    private String password;
     private Book book;
     private boolean hasBook;
 
     public User(String name, String password) {
         this.name = name;
-        this.password = password;
         this.book = null;
         this.hasBook = false;
     }
 
     public String getName() {
         return name;
-    }
-
-    public String getPassword() {
-        return password;
     }
 
     public Book getBook() {
@@ -34,10 +28,6 @@ public class User {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     public void setBook(Book book) {
@@ -53,7 +43,6 @@ public class User {
     public String toString(){
         return"User:{"
                 + "name=" + name
-                + ", password=" + password
                 + ", book=" + book
                 + ", hasBook=" + hasBook
                 + "}";
